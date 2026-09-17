fbe9fef76cf23a5b4fadb41b27cbf3913e172d87
Frank Carvalho <65919656+FrankSCarvalho@users.noreply.github.com>
2026-09-16 19:24:55 -0500

    Update materia.py

--- src/plataforma/persistencia/materia.py
+++ src/plataforma/persistencia/materia.py
@@ -1,6 +1,6 @@
-from src.plataforma.dominio import materia
+from src.plataforma.dominio.materia import Materia
 
 def salvar_materia(conexao, materia):
     conexao.execute("INSERT INTO materia (id, nome) VALUES (?, ?)", (materia.id, materia.nome))
-    
+
     conexao.commit()
